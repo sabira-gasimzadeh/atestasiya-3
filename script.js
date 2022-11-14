2172f28b4096c466e5f4bd9044df219fd2168add
sabira-gasimzadeh <sabiragasimzada@gmail.com>
2022-11-14 18:34:00 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+document.querySelector('#push').onclick = function(e){
+    let now = new Date();
+    e.preventDefault();
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert("Kindly Enter Task Name!!!!")
+    }
+
+    else{
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                ${now.getHours()}:${now.getMinutes()}
+              ${document.querySelector('#newtask input').value}
+                </span>
+            </div>
+        `;
+
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+    }
+}
